999c0d20f8c9dfb80f79d6f5359db0d9537af779
DIEGO ENMANUEL ZEPEDA RENDEROS <USSS017922@ugb.edu.sv>
2022-08-20 12:20:15 -0600

    Agregar un temporizador

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,11 @@ namespace Semana4_juego_de_parejas_CSharp
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null;
+
         
+        Label secondClicked = null;
+
         Random random = new Random();
 
         
@@ -52,18 +56,46 @@ namespace Semana4_juego_de_parejas_CSharp
 
         private void label1_Click(object sender, EventArgs e)
         {
+            
+            if (timer1.Enabled == true)
+                return;
+
             Label clickedLabel = sender as Label;
 
             if (clickedLabel != null)
             {
-                // If the clicked label is black, the player clicked
-                // an icon that's already been revealed --
-                // ignore the click
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
 
-                clickedLabel.ForeColor = Color.Black;
+                
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                
+                timer1.Start();
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            timer1.Stop();
+
+            
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            
+            firstClicked = null;
+            secondClicked = null;
         }
     }
 }
